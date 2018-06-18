b2cb17fc04e3555ff73b9d597c31c805ebf5cc70
Fabian Bächli <baechli.fabian@gmail.com>
2018-06-18 15:38:19 +0200

    changes in nlp

--- tweet_analyzer/implementation/tensorflow_testing/sentiment_analysis/main.py
+++ tweet_analyzer/implementation/tensorflow_testing/sentiment_analysis/main.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from os import listdir
 from os.path import isfile, join
 import re
+from random import randint
+import datetime
 
 wordsList = []
 with open('wordslist.txt', 'r') as file:
@@ -11,6 +13,19 @@ with open('wordslist.txt', 'r') as file:
 
 wordVectors = np.load('wordslist.npy')
 print ('Loaded the word vectors!')
+
+maxSeqLength = 10 #Maximum length of sentence
+numDimensions = 300 #Dimensions for each word vector
+firstSentence = np.zeros((maxSeqLength), dtype='int32')
+firstSentence[0] = wordsList.index("i")
+firstSentence[1] = wordsList.index("thought")
+firstSentence[2] = wordsList.index("the")
+firstSentence[3] = wordsList.index("movie")
+firstSentence[4] = wordsList.index("was")
+firstSentence[5] = wordsList.index("incredible")
+firstSentence[6] = wordsList.index("and")
+firstSentence[7] = wordsList.index("inspiring")
+#firstSentence[8] and firstSentence[9] are going to be 0
 
 positiveFiles = ['./pos/' + f for f in listdir('./pos/') if isfile(join('./pos/', f))]
 negativeFiles = ['./neg/' + f for f in listdir('./neg/') if isfile(join('./neg/', f))]
@@ -32,4 +47,64 @@ print('Negative files finished')
 numFiles = len(numWords)
 
 maxSeqLength = 250
+
 strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+def cleanSentences(string):
+  string = string.lower().replace("<br />", " ")
+  return re.sub(strip_special_chars, "", string.lower())
+
+ids = np.load('idsMatrix.npy')
+print('Loaded id\'s matrix')
+
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        if (i % 2 == 0): 
+            num = randint(1,11499)
+            labels.append([1,0])
+        else:
+            num = randint(13499,24999)
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+def getTestBatch():
+    labels = []
+    arr = np.zeros([batchSize, maxSeqLength])
+    for i in range(batchSize):
+        num = randint(11499,13499)
+        if (num <= 12499):
+            labels.append([1,0])
+        else:
+            labels.append([0,1])
+        arr[i] = ids[num-1:num]
+    return arr, labels
+
+batchSize = 24
+lstmUnits = 64
+numClasses = 2
+iterations = 100000
+
+tf.reset_default_graph()
+labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+input_data = tf.placeholder(tf.int32, [batchSize, maxSeqLength])
+data = tf.Variable(tf.zeros([batchSize, maxSeqLength, numDimensions]),dtype=tf.float32)
+data = tf.nn.embedding_lookup(wordVectors,input_data)
+lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+value = tf.transpose(value, [1, 0, 2])
+last = tf.gather(value, int(value.get_shape()[0]) - 1)
+prediction = (tf.matmul(last, weight) + bias)
+correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(labels,1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+tf.summary.scalar('Loss', loss)
+tf.summary.scalar('Accuracy', accuracy)
+merged = tf.summary.merge_all()
+logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
