a0330429a35e052591d5dc3a4030b7010df2877a
Fabian Bächli <baechli.fabian@gmail.com>
2018-07-04 22:23:01 +0200

    Update index.js

--- sentiment_classification_with_tf/implementation/index.js
+++ sentiment_classification_with_tf/implementation/index.js
@@ -2,10 +2,10 @@ const Twit = require('twit')
 const fs = require('fs')
 
 let twitterClient = new Twit({
-  consumer_key: 'iH7P4ru9f2VoerXf28gvs5XtK',
-  consumer_secret: '6NRTG1boNPvefQyNAy5MG2CTWyw8r0zW97VcpA31RhfJsLEHVB',
-  access_token: '999575621767516160-2B2pWuK7S4VoYBVnXXZMAvgxTA0SoLG',
-  access_token_secret: 'IaoiEjzjFIBRV8KBr2roCxPu63WAiT7hlCBNo6UjRf5xy'
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret
 })
 
 twitterClient.get('statuses/user_timeline', {screen_name: 'BarackObama'}, (err, data, response) => {
